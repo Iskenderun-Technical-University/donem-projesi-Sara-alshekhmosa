27ec611860d949061c90658d089b75895ccace15
Sara-alshekhmosa <sosetammmm@gmail.com>
2023-05-04 17:03:51 +0300

    sözleşme sayfasını ekledim

---
+++ Araç otomasyonu/sözleşme.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Araç_otomasyonu
+{
+    public partial class sözleşme : Form
+    {
+        public sözleşme()
+        {
+            InitializeComponent();
+        }
+
+        private void lblTc2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblEV_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2DateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGu2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
